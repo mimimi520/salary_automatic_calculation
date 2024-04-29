d38b165590069758c516ffe11ddce9acd4e10f71
mitsuhashi mizuki <mmpika052@gmail.com>
2024-04-29 18:53:41 +0900

    レビュー修正

--- app/controllers/salaries_controller.rb
+++ app/controllers/salaries_controller.rb
@@ -5,7 +5,7 @@ class SalariesController < ApplicationController
   
   def show
     @salary = Salary.find(params[:id]) 
-    @salary_amount = @salary["salary_amount"]
+    @salary_amount = @salary.salary_amount
     @result1 = (1030000 - @salary_amount)
     @result2 = (1030000 - @salary_amount)/12
   end
@@ -16,10 +16,10 @@ class SalariesController < ApplicationController
   end
   
   def create
-    @salaries = Salary.new(salary_params)
+    @salary = Salary.new(salary_params)
     
-    if @salaries.save
-      redirect_to salary_path(@salaries)
+    if @salary.save
+      redirect_to salary_path(@salary)
       
     end
   end
